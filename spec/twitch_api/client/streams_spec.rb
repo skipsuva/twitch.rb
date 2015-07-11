733e23d5fbff91da80d2e7fd98fe3e91853dd706
Spencer Rogers <spencer1248@gmail.com>
2015-07-10 22:35:40 -0400

    Spec for non-live streams

--- spec/twitch_api/client/streams_spec.rb
+++ spec/twitch_api/client/streams_spec.rb
@@ -9,5 +9,13 @@ describe TwitchApi::Client::Streams do
         assert_requested :get, twitch_url("/kraken/streams/Spencer124816")
       end
     end
+
+    context 'that is not live' do
+      it "returns the matching stream" do
+        stream = client.stream("#spencer1248")
+        expect(stream.game).to eq nil
+        assert_requested :get, twitch_url("/kraken/streams/Spencer124816")
+      end
+    end
   end
 end
