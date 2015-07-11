0d13d390b94ade1c69ea9d531f8f123bcb28f945
Spencer Rogers <spencer1248@gmail.com>
2015-07-10 22:33:52 -0400

    Instantiate client in spec

--- spec/twitch_api/client/streams_spec.rb
+++ spec/twitch_api/client/streams_spec.rb
@@ -1,8 +1,10 @@
 describe TwitchApi::Client::Streams do
+  let(:client) { TwitchApi::Client.new }
+
   describe ".stream", :vcr do
     context 'that is live' do
       it "returns the matching stream" do
-        stream = @client.stream("#spencer1248")
+        stream = client.stream("#spencer1248")
         expect(stream.game).to eq("Programming")
         assert_requested :get, twitch_url("/kraken/streams/Spencer124816")
       end
